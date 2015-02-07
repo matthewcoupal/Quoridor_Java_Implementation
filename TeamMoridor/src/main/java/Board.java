a4ee7c238d2e0d89d33603438447a733826893db
Matthew Coupal <coupalme198@potsdam.edu>
2015-02-06 22:34:00 -0500

    Added code in Board class to pass constructor with a number-of-players parameter.

--- TeamMoridor/src/main/java/Board.java
+++ TeamMoridor/src/main/java/Board.java
@@ -1,5 +1,23 @@
 package main.java;
 
-public class Board {
+import java.util.ArrayList;
 
+public class Board {
+	//Instance Variables
+	private ArrayList<Space> occupiedSpaces; //List of occupied spaces
+	private ArrayList<Edge> occupiedEdges; //List of wall locations
+	
+	public Board(int numberOfPlayers){
+		this.occupiedSpaces = new ArrayList<Space>();
+		this.occupiedEdges = new ArrayList<Edge>();
+		if(numberOfPlayers == 2) {
+			this.occupiedSpaces.add(new Space(5,0));
+			this.occupiedSpaces.add(new Space(5,9));
+		} else if (numberOfPlayers == 4) {
+			this.occupiedSpaces.add(new Space(5,0));
+			this.occupiedSpaces.add(new Space(5,9));
+			this.occupiedSpaces.add(new Space(0,5));
+			this.occupiedSpaces.add(new Space(9,5));
+		}
+	}
 }
