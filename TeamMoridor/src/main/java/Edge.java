7f6c1902d1091814095c64dabc5c641266564036
Matthew Coupal <coupalme198@potsdam.edu>
2015-02-06 19:42:42 -0500

    Created Edge class and added code to constructor to pass constructor with values test. Also added starting and ending coordinates for x and y values.

---
+++ TeamMoridor/src/main/java/Edge.java
@@ -0,0 +1,21 @@
+package main.java;
+
+public class Edge {
+	private int beginXCoordinate;
+	private int endXCoordinate;
+	private int beginYCoordinate;
+	private int endYCoordinate;
+	
+	// Parameter: beginX - Starting x coordinate
+	//			  beginY - Starting y coordinate
+	//			  endX - Ending x coordinate
+	//			  endY - Ending y coordinate
+	//Precondition: >= 0;
+	//Postconditon: Assigns Instance Variables their respective parameters.
+	public Edge(int beginX, int endX, int beginY, int endY){
+		this.beginXCoordinate = beginX;
+		this.beginYCoordinate = beginY;
+		this.endXCoordinate = endX;
+		this.endYCoordinate = endY;
+	}
+}
