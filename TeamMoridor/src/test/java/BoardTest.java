46c1440ef326cb432a7cdc36125bed9f6de944be
Matthew Coupal <coupalme198@potsdam.edu>
2015-02-06 22:15:59 -0500

    Created Board class and Board Test class. Created failing test for constructing 2 and 4 player board setups.

---
+++ TeamMoridor/src/test/java/BoardTest.java
@@ -0,0 +1,17 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import main.java.Board;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Test
+	public void boardCanBeConstructedWithValues() {
+		Board board1 = new Board(2);
+		Board board2 = new Board(4);
+	}
+
+}
