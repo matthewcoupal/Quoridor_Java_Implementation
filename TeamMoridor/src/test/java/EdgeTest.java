701487494d464809b46ef55b0c50c0f0c71e92df
Matthew Coupal <coupalme198@potsdam.edu>
2015-02-06 19:29:14 -0500

    Added new test file for Edge class (walls), with failing constructor (which takes coordinates).

---
+++ TeamMoridor/src/test/java/EdgeTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EdgeTest {
+
+	@Test
+	public void edgeCanBeConstructedWithValues() {
+		Edge line = new Edge(1,2,3,2);
+	}
+
+}
