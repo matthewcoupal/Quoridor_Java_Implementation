6c697701ce663416d9527a7983d71dfd89ed15d5
Matthew Coupal <coupalme198@potsdam.edu>
2015-02-12 21:42:33 -0500

    Added method to SpaceLinkedList class, along with working test, to add nodes to the linked list.

--- TeamMoridor/src/main/java/SpaceLinkedList.java
+++ TeamMoridor/src/main/java/SpaceLinkedList.java
@@ -4,8 +4,22 @@ public class SpaceLinkedList {
 
 	private SpaceNode front = null;
 	private int count = 0;
+	
 	public int size() {
 			return count;
+	}
+	public void add(SpaceNode newNode) {
+		if(front == null) {
+			front = newNode;
+			count = count + 1;
+		}else {
+			SpaceNode current = front;
+			while(current.getBottomNode() != null) {
+				current = current.getBottomNode();
+			}
+			current.setBottomNode(newNode);
+			count = count + 1;
+		}
 	}
 
 }
--- TeamMoridor/src/test/java/SpaceLinkedListTest.java
+++ TeamMoridor/src/test/java/SpaceLinkedListTest.java
@@ -2,7 +2,9 @@ package test.java;
 
 import static org.junit.Assert.*;
 
+import main.java.Space;
 import main.java.SpaceLinkedList;
+import main.java.SpaceNode;
 
 import org.junit.Test;
 
@@ -19,11 +21,14 @@ public class SpaceLinkedListTest {
 		assertEquals(0, list.size());
 	}
 	
-	/*
+	
 	@Test
 	public void spaceLinkedListCanAddSpaceNodes() {
 		SpaceLinkedList list = new SpaceLinkedList();
 		list.add(new SpaceNode(new Space(1,1)));
-	}*/
+		assertEquals(1, list.size());
+		list.add(new SpaceNode(new Space(2,2)));
+		assertEquals(2, list.size());
+	}
 
 }
