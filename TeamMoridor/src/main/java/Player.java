15753ec6f1192a8429e714c22cf0e6a0b9a866d2
Matthew Coupal <coupalme198@potsdam.edu>
2015-02-14 15:11:23 -0500

    Edited constructor and getter of walls left: Added the initial number of walls to the constructor and a getter to retrieve the number of walls left. (This also has tests, with the altered constructor test.)

--- TeamMoridor/src/main/java/Player.java
+++ TeamMoridor/src/main/java/Player.java
@@ -1,11 +1,34 @@
 package main.java;
 
+/**
+ * Emulates  a player on the board by utilizing the same information as a 
+ * space, but with extra behaviors and attributes.
+ * @author coupalme198
+ *
+ */
 public class Player extends Space {
 
-	//Inherits coordinateX and coordinateY
 	
-	public Player(int x, int y) {
+	//Inherits coordinate x and coordinate y
+	private int wallsLeft;
+	/**
+	 * Creates player class based on Space class constructor.
+	 * @param x    The x coordinate of the player
+	 * @param y    The y coordinate of the player
+	 * @param wallstoStart    The number of walls the players starts the game 
+	 *                         with
+	 */
+	public Player(int x, int y, int wallstoStart) {
 		super(x, y);
+		wallsLeft = wallstoStart;
+	}
+
+	/**
+	 * Getter method to the wallsLeft field.
+	 * @return    The number of wall the player has left to place.
+	 */
+	public int getWalls() {
+		return wallsLeft;
 	}
 
 }
--- TeamMoridor/src/test/java/PlayerTest.java
+++ TeamMoridor/src/test/java/PlayerTest.java
@@ -10,9 +10,15 @@ public class PlayerTest {
 
 	@Test
 	public void playerCanBeConstructed() {
-		Player player = new Player(4,5);
+		Player player = new Player(4,5,10);
 		assertEquals(player.getX(), 4);
 		assertEquals(player.getY(), 5);
+	}
+	
+	@Test
+	public void playerCanGetWallsLeft() {
+		Player player = new Player(4, 5, 10);
+		assertEquals(10, player.getWalls());
 	}
 
 }
