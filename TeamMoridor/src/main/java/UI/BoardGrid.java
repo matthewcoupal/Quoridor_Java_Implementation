4239e6fdad2a5fe31eb76172d460acc2bb5f9bad
Matthew Coupal <coupalme198@potsdam.edu>
2015-03-12 22:00:32 -0400

    Added method to update the GUIs occupied spaces: Created the an update method to the BoardGrid class (GUI) which changes the colors of the spaces listed in the occupiedSpaces list. Needed Implementation: Reseting the board to the unoccupied color, then updating.

--- TeamMoridor/src/main/java/UI/BoardGrid.java
+++ TeamMoridor/src/main/java/UI/BoardGrid.java
@@ -1,3 +1,5 @@
+package main.java.UI;
+
 import javax.swing.JFrame;
 import javax.swing.JButton; 
 
@@ -13,16 +15,21 @@ import main.java.Space;
 public class BoardGrid extends Board {
     JButton[][] boardgrid; // instance for Grid of Buttons
     JFrame frame=new JFrame(); // Instance for Frame
-    Board board = new Board();
+    //Board board = new Board();
     // Creates the BoardGrid.
     public static void main(String[] args) {
+    	
 	new BoardGrid(9,9);
     }
     
     
-    //Creates a 9*9 Grid Board with frame size of 1000*1000
+    /**
+     * Creates a 9*9 Grid Board with frame size of 1000*1000.
+     * @param breadth the size of the board in the y direction.
+     * @param length the size of the board in the x direction
+     */
     public BoardGrid(int breadth, int length){
-
+    	super();
 	//Set frame size to 1000*1000
 	frame.setSize(1000,1000);
 	
@@ -32,7 +39,7 @@ public class BoardGrid extends Board {
 	boardgrid = new JButton[breadth][length];
 	
 	for(int i = 0; i < length; i++){
-	    for(int j = 0; j< breadth; j++){
+	    for(int j = 0; j < breadth; j++){
 		final String coordinates = "(" + j + "," + i + ")";
 		final int x = j;
 		final int y = i;
@@ -41,7 +48,8 @@ public class BoardGrid extends Board {
 			public void actionPerformed(ActionEvent e) {
 			    //System.out.println(coordinates);
 			    Space space = new Space(x,y);
-				board.spaceToString(space);
+			    Board board = new Board();
+				String protocolString = board.spaceToString(space);
 				
 			}
 		});
@@ -53,7 +61,9 @@ public class BoardGrid extends Board {
 	//Program closes on exiting window.
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
-	frame.setVisible(true); 
+	frame.setVisible(true);
+	
+	this.updatePositions();
     }
     
     /**
@@ -64,13 +74,17 @@ public class BoardGrid extends Board {
     	button.setBackground(Color.ORANGE);
     }
     
-    /*
+    /**
+     * Checks the occupied spaces on the board and changes the color of those spaces.
+     */
     public void updatePositions () {
-    	for(int i = 0; i < this.occupiedSpaces;) {
-    		
+    	for(int i = 0; i < this.occupiedSpaces.size(); i++) {
+    		int xCoordinate = this.occupiedSpaces.get(i).getX();
+    		int yCoordinate = this.occupiedSpaces.get(i).getY();
+    		setBackground(boardgrid[xCoordinate][yCoordinate]);
     	}
     }
-    */
+    
 
     
 }
